29edb549ee345f35e12d3e4af365b88535f82045
Dennis Sandler <sandler.denis@gmail.com>
2024-06-20 22:41:04 -0400

    Deserialized and initialized TRT (runtime).

--- example-rvm.cpp
+++ example-rvm.cpp
@@ -1,5 +1,6 @@
 
 #include <NvInfer.h>
+#include <NvInferRuntime.h>
 #include <NvInferRuntimeBase.h>
 
 #include <cuda_runtime.h>
@@ -35,16 +36,93 @@ class Logger : public nvinfer1::ILogger
   }
 };
 
+bool LoadTRTEngineMatting( int argc, char* argv[]
+                         , Logger& logger
+			 , nvinfer1::IRuntime* pTrtRuntime
+			 , nvinfer1::ICudaEngine** ppTrtCudaEngine )
+{
+  if( argc < 2 )
+  {
+    logger.log( nvinfer1::ILogger::Severity::kERROR, "Must specify path to RVM TRT engine as first argument. Exitting." );
+    return false;
+  }
+
+  std::string strTrtEngineFilepath = argv[1];
+  logger.log( nvinfer1::ILogger::Severity::kINFO, (std::string("Loading Matting TRT Engine '") + strTrtEngineFilepath + "'").c_str() );
+
+  FILE* fileTrtEngine = fopen( strTrtEngineFilepath.c_str(), "rb" );
+  if( !fileTrtEngine )
+  {
+    logger.log( nvinfer1::ILogger::Severity::kERROR, (std::string("Bad filepath for TRT engine '") + strTrtEngineFilepath + "'. Exiting").c_str() );
+    return false;
+  }
+
+  // Get the file size
+  fseek( fileTrtEngine, 0, SEEK_END );
+  long fileSizeTrtEngine = ftell(fileTrtEngine);
+  fseek( fileTrtEngine, 0, SEEK_SET );
+
+  // Allocate memory to hold the file content
+  unsigned char *bufferTrtEngine = (unsigned char *)malloc( fileSizeTrtEngine) ;
+  if (!bufferTrtEngine)
+  {
+    logger.log( nvinfer1::ILogger::Severity::kERROR, "Failed to allocate memory for TRT engine");
+    fclose( fileTrtEngine );
+    return false;
+  }
+
+  // Read the file content into the allocated memory
+  size_t bytesRead = fread( bufferTrtEngine, 1, fileSizeTrtEngine, fileTrtEngine );
+  if( bytesRead != fileSizeTrtEngine )
+  {
+    logger.log( nvinfer1::ILogger::Severity::kERROR, ("Failed to read the TRT engine file '" + strTrtEngineFilepath + "'.").c_str() );
+    free( bufferTrtEngine );
+    fclose( fileTrtEngine );
+    return false;
+  }
+
+  fclose( fileTrtEngine );
+  logger.log( nvinfer1::ILogger::Severity::kINFO, (std::string("File size: ") + std::to_string(fileSizeTrtEngine) + " bytes.").c_str() );
+
+  // Deserialize
+  *ppTrtCudaEngine = pTrtRuntime->deserializeCudaEngine( bufferTrtEngine, fileSizeTrtEngine );
+  free( bufferTrtEngine );
+  if( !ppTrtCudaEngine )
+  {
+    logger.log( nvinfer1::ILogger::Severity::kERROR, ("Invalid TRT engine file '" + strTrtEngineFilepath + "'.").c_str() );
+    return false;
+  }
+
+  logger.log( nvinfer1::ILogger::Severity::kINFO, ("Successfully loaded Matting TRT Engine '" + strTrtEngineFilepath + "'.").c_str() );
+
+  return true;
+}
+
+#define ERROR_TRT_RUNTIME_INIT_FAILED         1
+#define ERROR_TRT_ENGINE_FAILED_LOAD          2
 
 int main( int argc, char* argv[] )
 {
   Logger logger;
   logger.log( nvinfer1::ILogger::Severity::kINFO, "C++ TensorRT RVM Inference example" );
 
-  for( int i = 0; i < argc; i++ )
+  nvinfer1::IRuntime* pTrtRuntime = nvinfer1::createInferRuntime( logger );
+  if( !pTrtRuntime )
   {
-    std::cout << "arg[" << i << "] = " << argv[i] << std::endl;
+    logger.log( nvinfer1::ILogger::Severity::kERROR, "Failed to initialize TRT Runtime");
+    return ERROR_TRT_RUNTIME_INIT_FAILED;
   }
+  logger.log( nvinfer1::ILogger::Severity::kINFO, "TRT Runtime Created." );
+
+  nvinfer1::ICudaEngine* pTrtCudaEngine = nullptr;
+  if( !LoadTRTEngineMatting( argc, argv, logger, pTrtRuntime, &pTrtCudaEngine ) )
+  {
+    return ERROR_TRT_ENGINE_FAILED_LOAD;
+  }
+
+
+  pTrtCudaEngine->destroy();
+  pTrtRuntime->destroy();
 
   return 0;
 }
